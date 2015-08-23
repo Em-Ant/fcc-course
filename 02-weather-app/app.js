12e92fe9c016889ea5a6c799a136e8aa167ffc48
EmAnt <joshua999x@gmail.com>
2015-08-23 14:20:33 +0200

    Weather - Add error handling and rename bg images

--- 02-weather-app/app.js
+++ 02-weather-app/app.js
@@ -40,45 +40,63 @@ $(document).ready(function(){
       hue = 250 - Math.round(6*data.main.temp);
     }    
     
-    if( Date.now() >= data.sys.sunrise*1000 && Date.now() <= data.sys.sunset*1000)
-      var iconSwitch = '-day-';
-    else
-      iconSwitch = '-night-';
+      if( Date.now() >= data.sys.sunrise*1000 && Date.now() <= data.sys.sunset*1000)
+        var iconSwitch = '-day-';
+      else
+        iconSwitch = '-night-';
+
+    var resetHue = function(x){
+      x =  (x > 360) ? x -360 : x; 
+      return (x < 0) ? x + 360 : x;  
+    }
+
+    $('.view').fadeOut(function(){
+      $('#city').text(data.name+', '+data.sys.country);
+      $('#w-icon').removeClass().addClass("wi wi-owm"+iconSwitch+data.weather[0].id);
+      $('#D').text(data.weather[0].description);
+      $('#T').text(Math.round(data.main.temp*2)/2);
+      $("#T-units").removeClass().addClass(tempUnits);
+      $("#P").text(data.main.pressure);
+      $("#H").text(data.main.humidity);
+      $("#W").text(Math.round(data.wind.speed*2)/2);
+      $("#W-dir").removeClass().addClass('wi wi-wind towards-'+Math.round(data.wind.deg)+'-deg');
+      $("#W-units").text(speedUnits);
+      $('.view').css('background','linear-gradient(to bottom , hsl('+resetHue(hue)+',70%,20%), hsl('+resetHue(hue-60)+',80%,40%))');
+      $('.view').fadeIn();
+    });
     
-  var resetHue = function(x){
-    x =  (x > 360) ? x -360 : x; 
-    return (x < 0) ? x + 360 : x;  
-  }
-      
-  $('.view').fadeOut(function(){
-    $('#city').text(data.name+', '+data.sys.country);
-    $('#w-icon').removeClass().addClass("wi wi-owm"+iconSwitch+data.weather[0].id);
-    $('#D').text(data.weather[0].description);
-    $('#T').text(Math.round(data.main.temp*2)/2);
-    $("#T-units").removeClass().addClass(tempUnits);
-    $("#P").text(data.main.pressure);
-    $("#H").text(data.main.humidity);
-    $("#W").text(Math.round(data.wind.speed*2)/2);
-    $("#W-dir").removeClass().addClass('wi wi-wind towards-'+Math.round(data.wind.deg)+'-deg');
-    $("#W-units").text(speedUnits);
-    $('.view').css('background','linear-gradient(to bottom , hsl('+resetHue(hue)+',70%,20%), hsl('+resetHue(hue-60)+',80%,40%))');
-    $('.view').fadeIn();
-  });
-    // if it's the startUp query select the appropriate BG
+    /* if it's the startUp query select the appropriate BG. I used low-res images 
+    to save bandwidth and improve loading time. They can be replaced with better ones,
+    for production version*/
     if(firstQuery){
       if(data.main.temp > 30)
-        $('body').css('background-image','url(./img/desert.jpg)');
+        $('body').css('background-image','url(./img/w_bg_4.jpg)');
       else if (data.main.temp > 20)
-        $('body').css('background-image','url(./img/fair.jpg)');
+        $('body').css('background-image','url(./img/w_bg_3.jpg)');
       else if (data.main.temp > 10)
-        $('body').css('background-image','url(./img/foggy.jpg)');
+        $('body').css('background-image','url(./img/w_bg_2.jpg)');
       else
-        $('body').css('background-image','url(./img/cold.jpg)');
+        $('body').css('background-image','url(./img/w_bg_1.jpg)');
       $('.preloader').fadeOut();
       firstQuery = false;
     }    
     $('body').css('background-size','cover')
   };
+  
+  function displayErr(){
+    $('.view').hide();
+    $('#city').text('------');
+    $('#w-icon').removeClass().addClass('wi wi-na');
+    $('#D').text('Connection Error');
+    $('#T').text('--');
+    $("#T-units").removeClass();
+    $("#P").text('----');
+    $("#H").text('---');
+    $("#W").text('---');
+    $("#W-dir").removeClass().addClass('wi wi-wind');
+    $("#W-units").text('---');
+    $('.view').fadeIn();    
+  }
   
   /* toggle units and perform a new query for weather
   I could have written a func to convert values, 
@@ -90,11 +108,11 @@ $(document).ready(function(){
       queryOptions.units = 'metric';
     else
       queryOptions.units = 'imperial';
-    getWeather().then(displayData);
+    getWeather().then(displayData,displayErr);
   });
   
   // startUp API queries
-  getPosition().then(getWeather).then(displayData);
+  getPosition().then(getWeather,displayErr).then(displayData,displayErr);
   
 });
 
