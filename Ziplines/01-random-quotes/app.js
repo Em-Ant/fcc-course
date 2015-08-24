86ba6f35368d51701d3b50a55627c75d41a35145
EmAnt <joshua999x@gmail.com>
2015-08-24 11:42:03 +0200

    Zipline Random Quote - change service and program structure

--- Ziplines/01-random-quotes/app.js
+++ Ziplines/01-random-quotes/app.js
@@ -1,53 +1,57 @@
 $(document).ready(function() {
-     
-  /* function that gets a new quote form STANDS4 and configures the tweet btn */
-  var new_quote = function() {
+
+  var setLoadingState=function(){
+    var deferred = $.Deferred();
     $('#new').attr('disabled','disabled');
     $('#twt').attr('disabled','disabled');
-    $("#quote-area div").addClass('dead').slideUp();
-    $('.loader').fadeIn();
-    var $div;
-    var q = $.ajax({
-      url : "http://www.stands4.com/services/v2/quotes.php?uid=4203&tokenid=WlHjiz0FePRFOxH1&searchtype=RANDOM",
-      success: function(){          
-        $div = $("<div></div>");
-        var $xml = $($.parseXML(q.responseText));   
-        $div.hide();
-        console.log('succ');
-        $('.dead').remove();
-        var quote = $xml.find("quote").text();
-        var author = $xml.find("author").text()
-        $div.append($('<h3 class="quote"></h3>').text('"'+quote+'"'));
-        $div.append($('<h4 class="auth"></h4>').text("- "+author));
-        $("#quote-area").append($div);
-        var tw_quote;     
-        // slice quote string for twitter
-        if (quote.length > 125-author.length){
-          tw_quote = quote.slice(0,122-author.length) +"...";
-          console.log(tw_quote);
-        }
-         else
-           tw_quote = quote;
-        $('#twt').attr('href','https://twitter.com/intent/tweet?text='+encodeURIComponent(tw_quote+"["+author+"]")+"%20%23RandomQuote");
-        $('#new').removeAttr('disabled');
-        $('#twt').removeAttr('disabled');
-        $('.loader').fadeOut(function(){$div.slideDown(500);});
-        },
-      error: function(){
-        $div = $("<div></div>");
-        $div.hide();
-        $('.dead').remove();
-        $div.append($("<h3></h3>").text("Connection Error !"));
-        $('#twt').attr('href','https://twitter.com/intent/tweet?text=%20%23RandomQuote'); 
-        $('#new').removeAttr('disabled');
-        $('#twt').removeAttr('disabled');
-        $('.loader').fadeOut(function(){$div.slideDown(500);});     
-      }
+    $("#quote-area").slideUp(function(){
+      $('.loader').fadeIn(function(){
+        deferred.resolve();
+      });
     });
+    return deferred.promise();                      
+  };
+  
+  var setQuote = function(data){
+    var quote = data.quoteText;
+    var author = data.quoteAuthor;
+    $('#Q').text('"'+quote+'"');
+    $('#A').text(author);
+    var tw_quote;     
+    // slice quote string for twitter
+    if (quote.length > 125-author.length){
+      tw_quote = quote.slice(0,122-author.length) +"...";
+    }
+    else
+      tw_quote = quote;
+    $('#twt').attr('href','https://twitter.com/intent/tweet?text='+encodeURIComponent(tw_quote+"["+author+"]")+"%20%23RandomQuote");
+  };
+
+  var setError = function(){
+    $('#Q').text('Connection Error');
+    $('#A').text('~');
+    $('#twt').attr('href','https://twitter.com/intent/tweet?text=%20%23RandomQuote');     
+  };
+  
+  var getData = function(){
+    return $.ajax({
+          url : "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+          dataType:'json',
+          jsonp : 'jsonp',
+    });
+  };
+  
+  var setNormalStatus = function(){
+    $('#new').removeAttr('disabled');
+    $('#twt').removeAttr('disabled');
+    $('.loader').fadeOut(function(){$('#quote-area').slideDown()});
+  };
+  
+  var new_quote = function() {
+    setLoadingState().then(getData).then(setQuote,setError).always(setNormalStatus);    
   };
   
   /* Bind click event on main btn, and load the first quote */
   $("#new").click(new_quote);
-  $('.loader').fadeIn();
   new_quote();
 });
